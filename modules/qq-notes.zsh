4ee2df556638596d8fb0638fbc286cc45291f344
Twilight4 <electrolight071@gmail.com>
2023-12-24 22:16:24 +0100

    update

--- modules/qq-notes.zsh
+++ modules/qq-notes.zsh
@@ -14,7 +14,9 @@ stored in a directory specified by the __NOTES environment variable (qq-vars-glo
 Commands
 --------
 qq-notes-install     installs dependencies
-qq-notes             lists all notes in $__NOTES or searches notes by filename if $1 is supplied
+qq-notes             lists all notes in $__NOTES and cats output or searches notes by stage if argument is supplied
+qq-notes-edit        lists all notes in $__NOTES and edits or searches notes by stage if argument is supplied
+
 qq-notes-content     list all notes in $__NOTES or searches notes by content if $1 is supplied
 qq-notes-menu        display an interactive menu for reading notes
 
@@ -35,12 +37,66 @@ qq-notes-install() {
 
 qq-notes() {
     __check-notes
-    __info "Use \$1 to search file names"
-    select note in $(\ls -R --file-type ${__NOTES} | grep -ie ".org$" | grep -i "$1")
-    do test -n ${note} && break
-    exit
-    done
-	[[ ! -z ${note} ]] && sed -e 's/=\([^=]*\)=/\o033[1;32m\1\o033[0m/g; s/^\( \{0,6\}\)-/•/g' -e '/^\(:PROPERTIES:\|:ID:\|:END:\|#\+date:\)/d' ${__NOTES}/${note} | command bat --language=org --style=plain --color=always
+	
+	arg="$1"
+
+    # Check if the argument is "ad"
+    if [ "$arg" = "ad" ]; then
+        # If "ad" is provided, go to active-directory
+        pushd "${__NOTES}/active-directory" &> /dev/null
+    elif [ "$arg" = "ad-en" ]; then
+        pushd "${__NOTES}/active-directory/domain-enumeration" &> /dev/null
+    elif [ "$arg" = "ad-ex" ]; then
+        pushd "${__NOTES}/active-directory/exploitation" &> /dev/null
+    elif [ "$arg" = "ad-pe" ]; then
+        pushd "${__NOTES}/active-directory/post-exploitation" &> /dev/null
+    elif [ "$arg" = "pen" ]; then
+        # If "pen" is provided, go to network-pentest directory
+        pushd "${__NOTES}/network-pentest" &> /dev/null
+    elif [ "$arg" = "pen-en" ]; then
+        pushd "${__NOTES}/network-pentest/enumeration" &> /dev/null
+    elif [ "$arg" = "pen-ex" ]; then
+        pushd "${__NOTES}/network-pentest/exploitation" &> /dev/null
+    elif [ "$arg" = "pen-pe" ]; then
+        pushd "${__NOTES}/network-pentest/post-exploitation" &> /dev/null
+    elif [ "$arg" = "red" ]; then
+        # If "red" is provided, go to red-team directory
+        pushd "${__NOTES}/red-team" &> /dev/null
+    elif [ "$arg" = "red-re" ]; then
+        pushd "${__NOTES}/red-team/recon" &> /dev/null
+    elif [ "$arg" = "red-we" ]; then
+        pushd "${__NOTES}/red-team/weaponization" &> /dev/null
+    elif [ "$arg" = "red-in" ]; then
+        pushd "${__NOTES}/red-team/initial-access" &> /dev/null
+    elif [ "$arg" = "red-pe" ]; then
+        pushd "${__NOTES}/red-team/post-exploitation" &> /dev/null
+    elif [ "$arg" = "red-ao" ]; then
+        pushd "${__NOTES}/red-team/action-on-objectives" &> /dev/null
+    else
+        # Otherwise, go to the default notes directory
+        pushd "${__NOTES}" &> /dev/null
+    fi
+
+	# don't do ls after cd
+	export ENHANCD_HOOK_AFTER_CD=""
+
+    # Shift the processed argument
+    shift
+
+	# select a directory of notes using fzf
+	cd .
+
+	# select with preview which note to output
+	selected_file=$(find . -type f | fzf --query="$1" --no-multi --select-1 --exit-0 \
+    --reverse --bind "ctrl-q:preview-down,alt-q:preview-up,ctrl-f:preview-page-down,ctrl-b:preview-page-up" --preview 'bat --language=org --style=numbers --color=always --line-range :500 {}')
+  
+	# if file selected, output it
+	if [[ -n "$selected_file" ]]; then
+		sed -e 's/=\([^=]*\)=/\o033[1;32m\1\o033[0m/g; s/^\( \{0,6\}\)-/•/g' -e '/^\(:PROPERTIES:\|:ID:\|:END:\|#\+date:\)/d' "$file" | command bat --language=org --style=plain --color=always
+	fi
+	
+	# return to directory
+	popd &> /dev/null
 }
 
 qq-notes-content() {
