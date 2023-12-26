8fd6c1e41797e028c4bdee8c4b42a4b58eaec605
Twilight4 <electrolight071@gmail.com>
2023-12-26 16:57:13 +0100

    update

--- modules/nb-notes.zsh
+++ modules/nb-notes.zsh
@@ -34,51 +34,54 @@ nb-notes-install() {
 
 nb-notes() {
     __check-notes
-	
-	arg="$1"
 
-    # Check if the argument is "ad"
-    if [ "$arg" = "ad" ]; then
-        # If "ad" is provided, go to active-directory
-        pushd "${__NOTES}/active-directory" &> /dev/null
-    elif [ "$arg" = "ad-en" ]; then
-        pushd "${__NOTES}/active-directory/1-domain-enumeration" &> /dev/null
-    elif [ "$arg" = "ad-ex" ]; then
-        pushd "${__NOTES}/active-directory/2-exploitation" &> /dev/null
-    elif [ "$arg" = "ad-pe" ]; then
-        pushd "${__NOTES}/active-directory/3-post-exploitation" &> /dev/null
-    elif [ "$arg" = "pen" ]; then
-        # If "pen" is provided, go to network-pentest directory
-        pushd "${__NOTES}/network-pentest" &> /dev/null
-    elif [ "$arg" = "pen-en" ]; then
-        pushd "${__NOTES}/network-pentest/1-enumeration" &> /dev/null
-    elif [ "$arg" = "pen-ex" ]; then
-        pushd "${__NOTES}/network-pentest/2-exploitation" &> /dev/null
-    elif [ "$arg" = "pen-pe" ]; then
-        pushd "${__NOTES}/network-pentest/3-post-exploitation" &> /dev/null
-    elif [ "$arg" = "red" ]; then
-        # If "red" is provided, go to red-team directory
-        pushd "${__NOTES}/red-team" &> /dev/null
-    elif [ "$arg" = "red-re" ]; then
-        pushd "${__NOTES}/red-team/1-recon" &> /dev/null
-    elif [ "$arg" = "red-we" ]; then
-        pushd "${__NOTES}/red-team/2-weaponization" &> /dev/null
-    elif [ "$arg" = "red-in" ]; then
-        pushd "${__NOTES}/red-team/3-initial-access" &> /dev/null
-    elif [ "$arg" = "red-pe" ]; then
-        pushd "${__NOTES}/red-team/4-post-exploitation" &> /dev/null
-    elif [ "$arg" = "red-ao" ]; then
-        pushd "${__NOTES}/red-team/5-action-on-objectives" &> /dev/null
-    else
-        # Otherwise, go to the default notes directory
-        pushd "${__NOTES}" &> /dev/null
+    # Check if at least one argument is provided
+    if [ $# -gt 0 ]; then
+        arg="$1"
+
+        # Check if the argument is "ad"
+        if [ "$arg" = "ad" ]; then
+            # If "ad" is provided, go to active-directory
+            pushd "${__NOTES}/active-directory" &> /dev/null
+        elif [ "$arg" = "ad-en" ]; then
+            pushd "${__NOTES}/active-directory/1-domain-enumeration" &> /dev/null
+        elif [ "$arg" = "ad-ex" ]; then
+            pushd "${__NOTES}/active-directory/2-exploitation" &> /dev/null
+        elif [ "$arg" = "ad-pe" ]; then
+            pushd "${__NOTES}/active-directory/3-post-exploitation" &> /dev/null
+        elif [ "$arg" = "pen" ]; then
+            # If "pen" is provided, go to network-pentest directory
+            pushd "${__NOTES}/network-pentest" &> /dev/null
+        elif [ "$arg" = "pen-en" ]; then
+            pushd "${__NOTES}/network-pentest/1-enumeration" &> /dev/null
+        elif [ "$arg" = "pen-ex" ]; then
+            pushd "${__NOTES}/network-pentest/2-exploitation" &> /dev/null
+        elif [ "$arg" = "pen-pe" ]; then
+            pushd "${__NOTES}/network-pentest/3-post-exploitation" &> /dev/null
+        elif [ "$arg" = "red" ]; then
+            # If "red" is provided, go to red-team directory
+            pushd "${__NOTES}/red-team" &> /dev/null
+        elif [ "$arg" = "red-re" ]; then
+            pushd "${__NOTES}/red-team/1-recon" &> /dev/null
+        elif [ "$arg" = "red-we" ]; then
+            pushd "${__NOTES}/red-team/2-weaponization" &> /dev/null
+        elif [ "$arg" = "red-in" ]; then
+            pushd "${__NOTES}/red-team/3-initial-access" &> /dev/null
+        elif [ "$arg" = "red-pe" ]; then
+            pushd "${__NOTES}/red-team/4-post-exploitation" &> /dev/null
+        elif [ "$arg" = "red-ao" ]; then
+            pushd "${__NOTES}/red-team/5-action-on-objectives" &> /dev/null
+        else
+            # Otherwise, go to the default notes directory
+            pushd "${__NOTES}" &> /dev/null
+        fi
+
+        # Shift the processed argument
+        shift
     fi
 
 	# don't do ls after cd
 	export ENHANCD_HOOK_AFTER_CD=""
-
-    # Shift the processed argument
-    shift
 
 	# select a directory of notes using fzf
 	cd .
