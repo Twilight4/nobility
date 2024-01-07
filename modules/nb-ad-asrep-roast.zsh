e70bc6e0896a8e73f84a85bf1161fa56837536e9
Twilight4 <electrolight071@gmail.com>
2024-01-07 18:57:37 +0100

    update

---
+++ modules/nb-ad-asrep-roast.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+############################################################# 
+# nb-ad-asrep-roast
+#############################################################
+nb-ad-asrep-roast-help() {
+    cat << "DOC" | bat --plain --language=help
+
+nb-ad-asrep-roast
+------------
+
+Commands
+--------
+
+DOC
+}
--- modules/nb-ad-kerb.zsh
+++ modules/nb-ad-kerb.zsh
@@ -1,14 +1,14 @@
 #!/usr/bin/env zsh
 
 ############################################################# 
-# nb-enum-kerb
+# nb-ad-kerb
 #############################################################
 nb-ad-kerb-help() {
     cat << "DOC" | bat --plain --language=help
 
 nb-ad-kerb
 ------------
-The nb-enum-kerb namespace contains commands for scanning and 
+The nb-ad-kerb namespace contains commands for scanning and 
 enumerating kerberos records and servers.
 
 Commands
@@ -22,31 +22,31 @@ nb-ad-kerb-kerberoast     get SPN for a service account
 DOC
 }
 
-nb-enum-kerb-install() {
+nb-ad-kerb-install() {
     __info "Running $0..."
     __pkgs tcpdump nmap impacket-scripts   
 }
 
-nb-enum-kerb-nmap-sweep() {
+nb-ad-kerb-nmap-sweep() {
     __check-project
     nb-vars-set-network
     print -z "sudo nmap -n -Pn -sS -p88 ${__NETWORK} -oA $(__netpath)/kerb-sweep"
 }
 
-nb-enum-kerb-tcpdump() {
+nb-ad-kerb-tcpdump() {
     __check-project
     nb-vars-set-iface
     nb-vars-set-rhost
     print -z "sudo tcpdump -i ${__IFACE} host ${__RHOST} and tcp port 88 -w $(__hostpath)/kerb.pcap"
 }
 
-nb-enum-kerb-users() {
+nb-ad-kerb-users() {
     nb-vars-set-rhost
     local realm && __askvar realm REALM
     print -z "nmap -v -p 88 --script krb5-enum-users --script-args krb5-enum-users.realm=${realm},userdb=/usr/share/seclists/Usernames/Names/names.txt ${__RHOST}"
 }
 
-nb-enum-kerb-kerberoast() {
+nb-ad-kerb-kerberoast() {
     __ask "Enter target AD domain (must also be set in your hosts file)"
     nb-vars-set-domain
     __ask "Enter service user account"
