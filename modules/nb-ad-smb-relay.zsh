039c09ee16edcb9d2c805ec5404846338d5f4b87
Twilight4 <electrolight071@gmail.com>
2024-01-08 15:37:39 +0100

    update

--- modules/nb-ad-smb-relay.zsh
+++ modules/nb-ad-smb-relay.zsh
@@ -14,13 +14,10 @@ enumerating Active Directory DC, GC and LDAP servers.
 Commands
 --------
 nb-ad-smb-relay-install        installs dependencies
-nb-ad-smb-relay     scan a network for services
-nb-ad-smb-relay        capture traffic to and from a host
-nb-ad-smb-relay            query ldap naming contexts
-nb-ad-smb-relay    connect with anonymous bind and query ldap
-nb-ad-smb-relay    connect with authenticated bind and query ldap
-nb-ad-smb-relay         send ldap whoami request
-nb-ad-smb-relay          brute force passwords for a user account
+nb-ad-smb-relay-enum           identify hosts without smb signing
+nb-ad-smb-relay-responder      capture and replay the SMB requests
+nb-ad-smb-relay-multirelay     query ldap naming contexts
+nb-ad-smb-relay-ntlmrelay      connect with anonymous bind and query ldap
 
 DOC
 }
