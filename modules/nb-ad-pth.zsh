58ecd5eb01de3cffb50f2953f13cdfe40a5bbcdb
Twilight4 <electrolight071@gmail.com>
2024-01-08 08:07:04 +0100

    update

--- modules/nb-ad-pth.zsh
+++ modules/nb-ad-pth.zsh
@@ -8,10 +8,17 @@ nb-ad-pth-help() {
 
 nb-ad-pth
 ------------
-nb-ad-pth
+The
 
 Commands
 --------
+nb-ad-pth-install         installs dependencies
+nb-ad-pth-pass            pass the password
+nb-ad-pth-exploit         pth exploit command
+nb-ad-pth-sam             dump SAM hashes
+nb-ad-pth-enum            enumerate shares
+nb-ad-pth-lsa             use CME to dump LSA
+nb-ad-pth-lsassy          use CME to dump LSASSY
 
 DOC
 }
