31edb6eb541d5228ac59d77b3ca58611e188f394
Twilight4 <electrolight071@proton.me>
2024-04-04 00:21:29 +0200

    update

---
+++ modules/nb-crack.zsh
@@ -0,0 +1,59 @@
+#!/usr/bin/env zsh
+
+############################################################# 
+# nb-crack
+#############################################################
+nb-crack-help() {
+    cat << "DOC" | bat --plain --language=help
+
+nb-crack
+----------
+The crack namespace provides commands for crackign password hashes.
+
+Commands
+--------
+nb-crack-hash-classic          Classic hashcat usage to crack a hash
+
+DOC
+}
+
+nb-crack-hash() {
+	__ask "Enter the hash"
+	__check-hash
+	__ask "Enter a password wordlist"
+	nb-vars-set-passlist
+
+  # Capture the output of hashid command and extract the third line
+  ht=$(hashid ${__HASH} | awk 'NR==3{print $2}')
+  __info "Hash type: $ht"
+
+  # Determine hash mode based on hash type
+  if [[ $ht == *"MD5"* ]]; then
+      md=0
+  elif [[ $ht == *"SHA-1"* ]]; then
+      md=100
+  elif [[ $ht == *"SHA-256"* ]]; then
+      md=1400
+  elif [[ $ht == *"NTLM"* ]]; then
+      md=1000
+  elif [[ $ht == *"LM"* ]]; then
+      md=3000
+  elif [[ $ht == *"NTLMv1"* ]]; then
+      md=5500
+  elif [[ $ht == *"NTLMv2"* ]]; then
+      md=5600
+  elif [[ $ht == *"MS-Cache"* ]]; then
+      md=11000
+  elif [[ $ht == *"DCC"* ]]; then
+      md=2100
+  elif [[ $ht == *"WPA"* ]]; then
+      md=2500
+  # Add more conditions for other hash types as needed
+  else
+      __warn "Hash type not recognized. Enter hashcat mode for the hash type."
+      local md && __askvar md "HASHCAT MODE"
+  fi
+
+  echo
+  print -z "hashcat -a 0 -m $md ${__HASH} ${__PASSLIST}"
+}
