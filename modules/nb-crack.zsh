516166fe83426d91539c2af67f703c8f58785c9f
Twilight4 <electrolight071@proton.me>
2024-04-14 16:14:46 +0200

    update

--- modules/nb-crack.zsh
+++ modules/nb-crack.zsh
@@ -56,6 +56,9 @@ nb-crack-hashcat() {
       md=2500
   elif [[ $ht == *"Kerberos 5"* ]]; then
       md=13100
+  # This one is for hashes from performing asreproasting attack
+  #elif [[ $ht == *"Kerberos 5, etype 23, AS-REP"* ]]; then
+  #    md=18200
   else
       # Add more conditions for other hash types as needed
       __warn "Hash type not recognized. Enter hashcat type for the hash mode:"
