0e3ed0b84f1f9e7d77f4b017280c6528d6d1896e
Twilight4 <electrolight071@proton.me>
2024-05-05 18:29:28 +0200

    update

---
+++ modules/nb-ad-enum.zsh
@@ -0,0 +1,33 @@
+#!/usr/bin/env zsh
+
+############################################################# 
+# nb-ad-enum
+#############################################################
+nb-ad-enum-help() {
+    cat << "DOC" | bat --plain --language=help
+
+nb-ad-enum
+------------
+The nb-ad-enum namespace contains commands for enumerating Active Directory DC, GC and LDAP servers.
+
+Commands
+--------
+nb-ad-enum-install              Install dependencies
+nb-ad-enum-ldapdomaindump       Enumerate with LdapDomainDump
+nb-ad-enum-bloodhound           Enumerate with Bloodhound
+
+DOC
+}
+
+nb-ad-enum-install() {
+    __info "Running $0..."
+    __pkgs python3-ldapdomaindump bloodhound neo4j
+}
+
+nb-ad-enum-ldapdomaindump() {
+
+}
+
+nb-ad-enum-bloodhound() {
+
+}
