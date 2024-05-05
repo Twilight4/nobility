1a09911705668ad518e2a7a9b5a2bb1d4a405948
Twilight4 <electrolight071@proton.me>
2024-05-05 15:38:23 +0200

    update

--- modules/nb-ad-smb-relay.zsh
+++ modules/nb-ad-smb-relay.zsh
@@ -15,7 +15,8 @@ Commands
 --------
 nb-ad-smb-relay-install              installs dependencies
 nb-ad-smb-relay-enum                 identify hosts without smb signing
-nb-ad-smb-relay-responder            capture and replay the SMB requests
+nb-ad-smb-relay-responder            capture the SMB requests
+nb-ad-smb-relay-ntlmrelay            replay the SMB requests
 nb-ad-smb-relay-ntlmrelay-shell      get interactive shell
 nb-ad-smb-relay-ntlmrelay-whoami     execute a whoami shell command on a target host using ntlmrelayx.py
 nb-ad-smb-relay-multirelay-command   execute a shell command on a target host using multirelay.py
