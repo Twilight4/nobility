6f7a44e2a3d9df565a8d32f41c38db38b1203ddd
Twilight4 <electrolight071@proton.me>
2024-05-11 07:49:09 +0200

    update

--- modules/nb-enum-web.zsh
+++ modules/nb-enum-web.zsh
@@ -81,6 +81,14 @@ nb-enum-web-vhosts-gobuster() {
     print -z "gobuster vhost -u ${__URL} -w ${w} -a \"${__UA}\" -t ${__THREADS} -o $(__urlpath)/vhosts.txt"
 }
 
+nb-enum-web-vhosts-ffuf() {
+    __check-project
+    nb-vars-set-url
+    local w && __askpath w FILE /usr/share/seclists/Discovery/DNS/subdomains-top1mil-20000.txt
+    __check-threads
+    print -z "ffuf -c -p 0.1 -fs 612 -w ./vhost -u ${__URL} -w ${w} -a \"${__UA}\" -t ${__THREADS} -H 'HOST: FUZZ.${__URL}' -o $(__urlpath)/vhosts.txt -of csv"
+}
+
 
 ############################################################# 
 # screenshots
