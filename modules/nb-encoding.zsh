8ff1e17483f27c791b71c98ad925e0c2d13cefb0
Twilight4 <electrolight071@proton.me>
2024-05-12 10:37:22 +0200

    update

--- modules/nb-encoding.zsh
+++ modules/nb-encoding.zsh
@@ -12,11 +12,28 @@ The encoding namespace provides commands for encoding and decoding values.
 
 Commands
 --------
-nb-encoding-file-to-b64       encodes plain text file to base64, optional $1 as file
-nb-encoding-file-from-b64     decodes base64 file to plain text, optional $1 as file
+nb-encoding-to-b64-powershell    copy powershell command to encode a file to base64
+nb-encoding-to-b64               encodes plain text file to base64, optional $1 as file
+nb-encoding-from-b64             decodes base64 file to plain text, optional $1 as file
 
 DOC
 }
+
+
+nb-encoding-to-b64-powershell() {
+  __check-project
+  local path && __askvar path "FULL_PATH_TO_FILE"
+
+  echo
+  __COMMAND="[Convert]::ToBase64String((Get-Content -path "$path" -Encoding byte))"
+  echo "$__COMMAND" | wl-copy
+
+  __info "Command copied to clipboard"
+}
+
+
+
+
 
 nb-encoding-file-to-b64() {
     if [ "$#" -eq  "1" ]
