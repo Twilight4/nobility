5adb6df2f25e691932e3088b18995b564b2ce2fa
Twilight4 <electrolight071@proton.me>
2024-05-12 13:59:15 +0200

    update

--- modules/nb-encoding.zsh
+++ modules/nb-encoding.zsh
@@ -15,13 +15,25 @@ Commands
 nb-encoding-to-b64-powershell    copy powershell command to encode a file to base64
 nb-encoding-to-b64               encodes plain text file to base64, optional $1 as file
 nb-encoding-from-b64             decodes base64 file to plain text, optional $1 as file
+nb-encrypt-aes256                encrypt file with a password, using openssl and aes256 cipher
+nb-decrypt-aes256                decrypt aes256 password protected file using openssl
 
 DOC
 }
 
+nb-encrypt-openssl() {
+  local filename && __askvar filename "FILENAME"
+
+  print -z "openssl enc -aes256 -iter 100000 -pbkdf2 -in $filename -out $filename.enc"
+}
+
+nb-decrypt-aes256() {
+  local filename && __askvar filename "FILENAME"
+
+  print -z "openssl enc -d -aes256 -iter 100000 -pbkdf2 -in $filename.enc -out $filename"
+}
 
 nb-encoding-to-b64-powershell() {
-  __check-project
   local path && __askvar path "FULL_PATH_TO_FILE"
 
   echo
