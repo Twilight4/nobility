ffcb1eb92751981b2b018dc0c4921d5deb0fa25c
Twilight4 <electrolight071@proton.me>
2024-05-19 15:11:29 +0200

    update

--- modules/nb-enum-smb.zsh
+++ modules/nb-enum-smb.zsh
@@ -12,9 +12,7 @@ The nb-enum-smb namespace contains commands for scanning and enumerating smb ser
 
 Automated Enumeration tools
 -------------------------------------
-nb-enum-smb-install                  installs dependencies
 nb-enum-smb-nmap-sweep               scan a network for services
-nb-enum-smb-tcpdump                  capture traffic to and from a host
 nb-enum-smb-null-enum4               enumerate with enum4linux
 nb-enum-smb-null-enum4-aggresssive   aggressively enumerate with enum4linux
 
@@ -42,7 +40,10 @@ nb-enum-smb-user-smbclient-connect   connect with an authenticated session
 Other Commands
 -------------------------------------
 nb-enum-smb-null-smbmap-download     download a file from a share
+nb-enum-smb-hydra                    brute force password/login for a user account
 
+nb-enum-smb-install                  installs dependencies
+nb-enum-smb-tcpdump                  capture traffic to and from a host
 nb-enum-user-smb-mount               mount an SMB share
 nb-enum-smb-responder                spoof and get responses using responder
 nb-enum-smb-net-use-null             print a net use statement for windows
@@ -88,6 +89,67 @@ nb-enum-smb-null-smbmap-download() {
   print -z "smbmap -H ${__RHOST} --download \"${__SHARE\\$file\"}"
 }
 
+nb-enum-smb-hydra() {
+    __check-project
+    nb-vars-set-rhost
+
+    __ask "You wanna brute force login/password/both? (l/p/b)"
+    local login && __askvar login "LOGIN_OPTION"
+
+    __ask "Is the service running on default port? (y/n)"
+    local df && __askvar df "DEFAULT_PORT"
+
+    if [[ $df == "n" ]]; then
+      __ask "Enter port number"
+      local pn && __askvar pn "PORT_NUMBER"
+    else
+      __err "SOMETHING WENT WRONG. Aborting"
+      exit 1
+    fi
+
+    if [[ $login == "p" ]]; then
+      nb-vars-set-user
+      if [[ $df == "n" ]]; then
+        print -z "hydra -l ${__USER} -P ${__PASSLIST} -s $pn -o $(__hostpath)/smb-hydra-brute.txt ${__RHOST} smb -t 64 -F"
+      else
+        print -z "hydra -l ${__USER} -P ${__PASSLIST} -o $(__hostpath)/smb-hydra-brute.txt ${__RHOST} smb -t 64 -F"
+      fi
+    elif [[ $login == "l" ]]; then
+      nb-vars-set-wordlist
+      nb-vars-set-pass
+      if [[ $df == "n" ]]; then
+        print -z "hydra -L ${__WORDLIST} -p ${__PASS} -s $pn -o $(__hostpath)/smb-hydra-brute.txt ${__RHOST} smb -t 64 -F"
+      else
+        print -z "hydra -L ${__WORDLIST} -p ${__PASS} -o $(__hostpath)/smb-hydra-brute.txt ${__RHOST} smb -t 64 -F"
+      fi
+    elif [[ $login == "b" ]]; then
+      __ask "Do you wanna manually specify wordlists? (y/n)"
+      local sw && __askvar sw "SPECIFY_WORDLIST"
+      if [[ $sw == "y" ]]; then
+        __ask "Select a user list"
+        __askpath ul FILE $HOME/desktop/projects/
+        __ask "Select a password list"
+        __askpath pl FILE $HOME/desktop/projects/
+
+        if [[ $df == "n" ]]; then
+          print -z "hydra -L $ul -P $pl -s $pn -o $(__hostpath)/smb-hydra-brute.txt ${__RHOST} smb -t 64 -F"
+        else
+          print -z "hydra -L ${__WORDLIST} -P ${__PASSLIST} -o $(__hostpath)/smb-hydra-brute.txt ${__RHOST} smb -t 64 -F"
+        fi
+      else
+        nb-vars-set-wordlist
+        if [[ $df == "n" ]]; then
+          print -z "hydra -L ${__WORDLIST} -P ${__PASSLIST} -s $pn -o $(__hostpath)/smb-hydra-brute.txt ${__RHOST} smb -t 64 -F"
+        else
+          print -z "hydra -L ${__WORDLIST} -P ${__PASSLIST} -o $(__hostpath)/smb-hydra-brute.txt ${__RHOST} smb -t 64 -F"
+        fi
+      fi
+    else
+      echo
+      __err "Invalid option. Please choose 'p' for password or 'l' for login."
+    fi
+}
+
 nb-enum-smb-user-smbmap() {
   nb-vars-set-rhost
   __check-user
@@ -98,6 +160,11 @@ nb-enum-smb-user-smbmap() {
 nb-enum-smb-null-enum4() {
   nb-vars-set-rhost
   print -z "enum4linux -a ${__RHOST} | tee $(__hostpath)/enumlinux.txt"
+}
+
+nb-enum-smb-null-enum4-aggresssive() {
+  nb-vars-set-rhost
+  print -z "enum4linux -A ${__RHOST} | tee $(__hostpath)/enumlinux.txt"
 }
 
 nb-enum-smb-null-cme-list() {
