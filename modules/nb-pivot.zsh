f1dfe89375f5a554c44b120b962ad36e754d4cc9
Twilight4 <electrolight071@proton.me>
2024-05-24 16:43:34 +0200

    update

--- modules/nb-pivot.zsh
+++ modules/nb-pivot.zsh
@@ -17,7 +17,6 @@ nb-pivot-mount-remote-sshfs           mounts a remote directory to local /mnt pa
 nb-pivot-ssh-dynamic-proxy            enable dynamic port forwarding with ssh
 nb-pivot-ssh-socks-proxy-msf          configure msf's local proxy
 nb-pivot-ssh-remote-to-local          forwards remote port to local port
-nb-pivot-ssh-remote-to-local-burp     forwards remote port 8080 to local port 8080
 nb-pivot-chisel                       # TODO use chisel for pivoting
 
 DOC
@@ -47,7 +46,7 @@ nb-pivot-ssh-dynamic-proxy() {
     print -z "ssh -D ${__LPORT} -CqN ${__USER}@${__RHOST}" 
 }
 
-nb-pivot-ssh-socks-proxy() {
+nb-pivot-ssh-socks-proxy-msf() {
     nb-vars-set-lport
     local sb && __askvar sb SUBNET
     __info "Add the proxy to proxychains4.conf using command:"
@@ -55,10 +54,11 @@ nb-pivot-ssh-socks-proxy() {
 
     print -z "msfconsole -q -n -x 'use auxiliary/server/socket_proxy; set SRVPORT ${__LPORT}; set SRVHOST 0.0.0.0; set version 4a; run'"
 
-    __info "Use post/multi/manage/autoroute to tell our socks_proxy module to route all the traffic via the meterpreter session"
+    __info "Then use post/multi/manage/autoroute to tell our socks_proxy module to route all the traffic via the meterpreter session"
     __ok "use post/multi/manage/autoroute; set SESSION 1; set SUBNET $sb; run"
-    __info "You can also add routes with autoroute in metepreter session: 'run autoroute -s <subnet>/23'"
+    __info "You can also add routes with autoroute in metepreter session: 'run autoroute -s $sb'"
     __info "Use 'run autoroute -p' to list the active routes"
+    __info "You can now use proxychains with e.g. nmap"
 }
 
 nb-pivot-ssh-remote-to-local() {
@@ -68,10 +68,3 @@ nb-pivot-ssh-remote-to-local() {
     nb-vars-set-lport
     print -z "ssh -R ${__LPORT}:127.0.0.1:${__RPORT} ${__USER}@${__RHOST}" 
 }
-
-nb-pivot-ssh-remote-to-local-burp() {
-    __check-user
-    nb-vars-set-rhost
-    print -z "ssh -R 8080:127.0.0.1:8080 ${__USER}@${__RHOST}"
-}
-
