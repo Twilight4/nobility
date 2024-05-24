14486c80b295933ba344db26c80d191aac606447
Twilight4 <electrolight071@proton.me>
2024-05-24 22:10:03 +0200

    update

--- modules/nb-pivot.zsh
+++ modules/nb-pivot.zsh
@@ -10,24 +10,21 @@ nb-pivot
 ------------
 The pivot namespace provides commands for using proxies for pivoting.
 
-Branching Out Tunnels
+Using Automated Tools
 ---------------------
 nb-pivot-dnscat2-server               create a encrypted channel session over dns protocol in txt records
 nb-pivot-dnscat2-client               command to establish a tunnel with to the running server on the attack host
 nb-pivot-chisel-server                socks5 tcp/udp tunneling via http protocol using ssh with chisel server
 nb-pivot-chisel-client                socks5 tcp/udp tunneling via http protocol using ssh with chisel client
+nb-pivot-rpivot-server                web server pivoting with rpivot server.py
+nb-pivot-rpivot-client                web server pivoting with rpivot client.py
+nb-pivot-sshuttle                     ssh pivoting with sshuttle (without the need of proxychains)
 
 Using Metasploit
 ----------------
 nb-pivot-msf-local-proxy              forwards local port to remote port using msf's local socks4 proxy
 nb-pivot-msf-remote-proxy             forwards local port to remote port
 nb-pivot-msf-reverse-proxy            forwards remote port to local port
-
-Using Automated Tools
-------------
-nb-pivot-sshuttle                     ssh pivoting with sshuttle (without the need of proxychains)
-nb-pivot-rpivot-server                web server pivoting with rpivot server.py
-nb-pivot-rpivot-client                web server pivoting with rpivot client.py
 
 Using SSH
 ---------
