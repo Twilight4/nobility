d2305d89b15ab8852a675e8f733a90f673c8a2f1
Twilight4 <electrolight071@proton.me>
2024-06-01 11:52:55 +0200

    update

---
+++ scripts/wordlist-profiling.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+# password profiling
+#pwdology
+#cupp
+#cewl
+
+# username profiling
+#username-anarchy
+#username-generator
+#linkedin2username
+
+# password rules
+# password policy
