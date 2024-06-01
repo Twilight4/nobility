cfd86eff9958712db470bfbe094bd860d9593163
Twilight4 <electrolight071@proton.me>
2024-06-01 11:42:43 +0200

    update

--- modules/nb-recon-user.zsh
+++ modules/nb-recon-user.zsh
@@ -26,18 +26,13 @@ DOC
 }
 
 nb-recon-user-socials() {
-    f_runlocally
-    clear
-    f_banner
-    
-    echo -e "${BLUE}RECON${NC}"
-    echo
     echo -n "First name: "
     read firstName
     
     # Check for no answer
     if [ -z $firstName ]; then
-         f_error
+        __err "You need to provide first name"
+        exit 1
     fi
     
     echo -n "Last name:  "
@@ -45,24 +40,25 @@ nb-recon-user-socials() {
     
     # Check for no answer
     if [ -z $lastName ]; then
-         f_error
+        __err "You need to provide last name"
+        exit 1
     fi
     
     xdg-open https://www.411.com/name/$firstName-$lastName/ &
-    sleep 2
+    sleep 1
     uripath="https://www.advancedbackgroundchecks.com/search/results.aspx?type=&fn=${firstName}&mi=&ln=${lastName}&age=&city=&state="
     xdg-open $uripath &
-    sleep 2
+    sleep 1
     xdg-open https://www.linkedin.com/pub/dir/?first=$firstName\&last=$lastName\&search=Search &
-    sleep 2
+    sleep 1
     xdg-open https://www.peekyou.com/$firstName%5f$lastName &
-    sleep 2
+    sleep 1
     xdg-open https://www.addresses.com/people/$firstName+$lastName &
-    sleep 2
+    sleep 1
     xdg-open https://www.spokeo.com/$firstName-$lastName &
-    sleep 2
+    sleep 1
     xdg-open https://twitter.com/search?q=%22$firstName%20$lastName%22&src=typd &
-    sleep 2
+    sleep 1
     xdg-open https://www.youtube.com/results?search_query=$firstName+$lastName &
 
     #tinder
