0435990b17a1bcac98424728d4bf9f8fbf4b5a35
Twilight4 <electrolight071@proton.me>
2024-06-01 10:53:19 +0200

    update

--- modules/nb-enum-web-fuzz.zsh
+++ modules/nb-enum-web-fuzz.zsh
@@ -18,9 +18,6 @@ nb-enum-web-fuzz-auth-basic-ffuf              brute force basic auth
 nb-enum-web-fuzz-auth-json-ffuf               brute force basic auth with json post
 nb-enum-web-fuzz-auth-post-ffuf               brute force auth with post
 nb-enum-web-fuzz-auth-post-wfuzz              brute force auth with post
-nb-enum-web-fuzz-hydra-get                    brute force auth with get
-nb-enum-web-fuzz-password-hydra-form-post     brute force auth password with post request
-nb-enum-web-fuzz-login-hydra-form-post        brute force auth login with post request
 
 DOC
 }
@@ -75,36 +72,3 @@ nb-enum-web-fuzz-auth-post-wfuzz() {
     print -z "wfuzz -c -w ${__PASSLIST} -d \"${uf}=${uv}&${pf}=FUZZ\" --sc 302 ${__URL}"
 }
 
-nb-enum-web-fuzz-hydra-get() {
-    nb-vars-set-rhost
-    __check-user
-    __ask "Enter the URI for the get request, ex: /path"
-    local uri && __askvar uri URI
-    print -z "hydra -l ${__USER} -P ${__PASSLIST} ${__RHOST} http-get ${uri} -F"
-}
-
-nb-enum-web-fuzz-password-hydra-form-post() {
-    nb-vars-set-rhost
-    nb-vars-set-passlist
-    __ask "Enter the URI for the post request, ex: /path"
-    local uri && __askvar uri URI
-    local uf && __askvar uf USER_FIELD
-    local pf && __askvar pf PASSWORD_FIELD
-    __ask "Enter the username which you wanna bruteforce"
-    local un && __askvar un USER_NAME
-    __ask "Enter the response value to check for failure"
-    local fm && __askvar fm FAILURE
-    print -z "hydra ${__RHOST} http-form-post \"${uri}:${uf}=^USER^&${pf}=^PASS^:${fm}\" -l ${un} -P ${__PASSLIST} -t 10 -w 30 -F"
-}
-
-nb-enum-web-fuzz-login-hydra-form-post() {
-    nb-vars-set-rhost
-    nb-vars-set-wordlist
-    __ask "Enter the URI for the post request, ex: /path"
-    local uri && __askvar uri URI
-    local uf && __askvar uf USER_FIELD
-    local pf && __askvar pf PASSWORD_FIELD
-    __ask "Enter the response value to check for failure"
-    local fm && __askvar fm FAILURE
-    print -z "hydra ${__RHOST} http-form-post \"${uri}:${uf}=^USER^&${pf}=^PASS^:${fm}\" -l ${__WORDLIST} -p test -t 10 -w 30 -F"
-}
