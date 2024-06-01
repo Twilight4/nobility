e0998a5749e509f2dd37c6244e20a7968c2c4b44
Twilight4 <electrolight071@proton.me>
2024-06-01 11:38:15 +0200

    update

---
+++ modules/nb-recon-user.zsh
@@ -0,0 +1,74 @@
+#!/usr/bin/env zsh
+
+############################################################# 
+# nb-recon-user
+#############################################################
+nb-recon-user-help() {
+    cat << "DOC" | bat --plain --language=help
+
+nb-recon-user
+-------------
+The nb-recon-user namespace provides commands to search for user data.
+
+Commands
+--------
+nb-recon-user-socials            command to open social media websites with the provided name
+nb-recon-user-email              command to check for valid email address
+nb-recon-user-geolocation        command to search for gelocation data
+nb-recon-user-images             command to search for images
+nb-recon-user-metadata           command to check for file metadata
+nb-recon-user-breached           command to search for breached data
+nb-recon-user-google             command to search for google dorks
+nb-recon-user-phone              command to check for valid phone number
+nb-recon-user-username           command to search for valid usernames
+
+DOC
+}
+
+nb-recon-user-socials() {
+    f_runlocally
+    clear
+    f_banner
+    
+    echo -e "${BLUE}RECON${NC}"
+    echo
+    echo -n "First name: "
+    read firstName
+    
+    # Check for no answer
+    if [ -z $firstName ]; then
+         f_error
+    fi
+    
+    echo -n "Last name:  "
+    read lastName
+    
+    # Check for no answer
+    if [ -z $lastName ]; then
+         f_error
+    fi
+    
+    xdg-open https://www.411.com/name/$firstName-$lastName/ &
+    sleep 2
+    uripath="https://www.advancedbackgroundchecks.com/search/results.aspx?type=&fn=${firstName}&mi=&ln=${lastName}&age=&city=&state="
+    xdg-open $uripath &
+    sleep 2
+    xdg-open https://www.linkedin.com/pub/dir/?first=$firstName\&last=$lastName\&search=Search &
+    sleep 2
+    xdg-open https://www.peekyou.com/$firstName%5f$lastName &
+    sleep 2
+    xdg-open https://www.addresses.com/people/$firstName+$lastName &
+    sleep 2
+    xdg-open https://www.spokeo.com/$firstName-$lastName &
+    sleep 2
+    xdg-open https://twitter.com/search?q=%22$firstName%20$lastName%22&src=typd &
+    sleep 2
+    xdg-open https://www.youtube.com/results?search_query=$firstName+$lastName &
+
+    #tinder
+}
+
+nb-recon-user-username() {
+    #sherlock
+    #whatsmyname
+}
