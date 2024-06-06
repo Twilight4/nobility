2c0c3d60126bd3f29fa00ea73c44603764ab9fd8
Twilight4 <electrolight071@proton.me>
2024-06-06 06:15:32 +0200

    update

---
+++ modules/nb-local-persist.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+############################################################# 
+# nb-local-persist
+#############################################################
+nb-local-persist-help() {
+    cat << "DOC" | bat --plain --language=help
+
+nb-local-persist
+----------------
+The nb-local-persist namespace contains commands for local persistence techniques in windows.
+
+DOC
+}
