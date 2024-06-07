3c79ba28ba42165b38ec94c57111752916b10ed2
Twilight4 <electrolight071@proton.me>
2024-06-07 10:40:00 +0200

    update

--- modules/nb-enum-smtp.zsh
+++ modules/nb-enum-smtp.zsh
@@ -14,6 +14,7 @@ Commands
 --------
 nb-enum-smtp-sweep          scan a network for mail services
 nb-enum-smtp-open-relay     check for open relay misconfiguration
+nb-enum-smtp-connect        connect to the smtp/pop3 services
 
 DOC
 }
@@ -21,11 +22,26 @@ DOC
 nb-enum-smtp-sweep() {
     __check-project
     nb-vars-set-network
-    print -z "sudo grc nmap -n -Pn -sV -sC --script smtp-commands -p25,143,110,465,587,993,995 ${__NETWORK} -oA $(__netpath)/mail-sweep"
+    print -z "sudo grc nmap -n -Pn -sV -sC --script smtp-commands -p25,143,110,465,587,993,995 ${__NETWORK} -oA $(__netpath)/smtp-sweep"
 }
 
 nb-enum-smtp-open-relay() {
     __check-project
     nb-vars-set-network
-    print -z "sudo grc nmap -n -p25 -Pn --script smtp-open-relay ${__NETWORK} -oA $(__netpath)/mail-sweep
+    print -z "sudo grc nmap -n -p25 -Pn --script smtp-open-relay ${__NETWORK} -oA $(__netpath)/smtp-open-relay-check"
+}
+
+nb-enum-smtp-connect() {
+    __check-project
+    nb-vars-set-rhost
+
+    # connect
+    print -z "telnet ${__RHOST} 25"
+
+    # informational
+    echo
+    __info "Try connecting to other MAIL services ports if they're open:"
+    __ok "telnet ${__RHOST} 110"
+    __ok "telnet ${__RHOST} 465"
+    __ok "telnet ${__RHOST} 995"
 }
