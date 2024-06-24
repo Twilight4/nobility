5f101c7cb0516e59e4e774f0cddcacbc5ff80848
Twilight4 <electrolight071@gmail.com>
2024-06-24 17:39:59 +0200

    update

--- modules/nb-ad-ldap.zsh
+++ modules/nb-ad-ldap.zsh
@@ -1,55 +1,80 @@
 #!/usr/bin/env zsh
 
 ############################################################# 
-# nb-ad-ldap
+# nb-enum-ldap
 #############################################################
-nb-ad-ldap-help() {
+nb-enum-ldap-help() {
     cat << "DOC" | bat --plain --language=help
 
-nb-ad-ldap
+nb-enum-ldap
 ------------
-The nb-ad-ldap namespace contains commands for scanning and 
+The nb-enum-ldap namespace contains commands for scanning and 
 enumerating Active Directory DC, GC and LDAP servers.
+
+Check Authentication
+--------------------
+nb-enum-ldap-nmap-sweep     scan a network for services
+nb-enum-ldap-search-anon    connect with anonymous bind and query ldap
+nb-enum-ldap-search-auth    connect with authenticated bind and query ldap
+
+Enumeration - Without Authentication
+------------------------------------
+nb-enum-ldap-search-pass-pol      retrieve password policy using ldapsearch
+nb-enum-ldap-search-anon-users    use ldap anonymous search to enumerate valid usernames
 
 Commands
 --------
-nb-ad-ldap-install        installs dependencies
-nb-ad-ldap-nmap-sweep     scan a network for services
-nb-ad-ldap-tcpdump        capture traffic to and from a host
-nb-ad-ldap-ctx            query ldap naming contexts
-nb-ad-ldapsearch-anon    connect with anonymous bind and query ldap
-nb-ad-ldapsearch-auth    connect with authenticated bind and query ldap
-nb-ad-ldap-whoami         send ldap whoami request
-nb-ad-ldap-hydra          brute force passwords for a user account
+nb-enum-ldap-install        installs dependencies
+nb-enum-ldap-tcpdump        capture traffic to and from a host
+nb-enum-ldap-ctx            query ldap naming contexts
+nb-enum-ldap-whoami         send ldap whoami request
+nb-enum-ldap-hydra          brute force passwords for a user account
 
 DOC
 }
 
-nb-ad-ldap-install() {
+nb-enum-ldap-search-anon-users() {
+    __check-project
+    nb-vars-set-domain
+	  __ask "Enter the IP address of the target DC server"
+    local dc && __askvar dc DC_IP
+    
+    print -z "ldapsearch -H ldap://$dc:389 -x -b \"DC=${__DOMAIN},DC=LOCAL\" -s sub \"(&(objectclass=user))\" | grep sAMAccountName: | cut -f2 -d\" \""
+}
+
+nb-enum-ldap-ldapsearch-pass-pol() {
+    __check-project
+    nb-vars-set-domain
+    nb-vars-set-rhost
+
+    print -z "ldapsearch -h ${__RHOST} -x -b \"DC=${__DOMAIN},DC=LOCAL\" -s sub "*" | grep -m 1 -B 10 pwdHistoryLength | tee $(__netadpath)/ldapsearch-pass-pol.txt"
+}
+
+nb-enum-ldap-install() {
     __info "Running $0..."
     __pkgs tcpdump nmap ldap-utils hydra
 }
 
-nb-ad-ldap-nmap-sweep() {
+nb-enum-ldap-nmap-sweep() {
     __check-project
     nb-vars-set-network
     print -z "sudo grc nmap -n -Pn -sS -sU -p389,636,3269 ${__NETWORK} -oA $(__netpath)/ldap-sweep"
 }
 
-nb-ad-ldap-tcpdump() {
+nb-enum-ldap-tcpdump() {
     __check-project
     nb-vars-set-iface
     nb-vars-set-rhost
     print -z "sudo tcpdump -i ${__IFACE} host ${__RHOST} and tcp port 389 and port 636 and port 3269 -w $(__hostpath)/ldap.pcap"
 }
 
-nb-ad-ldap-ctx() {
+nb-enum-ldap-ctx() {
     __ask "Enter the address of the target DC, GC or LDAP server"
     nb-vars-set-rhost
     print -z "ldapsearch -x -H ldap://${__RHOST}:389 -s base namingcontexts"
 }
 
-nb-ad-ldapsearch-anon() {
+nb-enum-ldapsearch-anon() {
     __ask "Enter the address of the target DC, GC or LDAP server"
     nb-vars-set-rhost
     __ask "Enter a distinguished name (DN), such as: 'dc=htb,dc=local'"
@@ -57,7 +82,7 @@ nb-ad-ldapsearch-anon() {
     print -z "ldapsearch -x -H ldap://${__RHOST}:389 -s sub -b \"${dn}\" "
 }
 
-nb-ad-ldapsearch-auth() {
+nb-enum-ldapsearch-auth() {
     __ask "Enter the address of the target DC, GC or LDAP server"
     nb-vars-set-rhost
     __ask "Enter a distinguished name (DN), such as: 'dc=htb,dc=local'"
@@ -67,13 +92,13 @@ nb-ad-ldapsearch-auth() {
     print -z "ldapsearch -x -H ldap://${__RHOST}:389 -D '${dn}' \"(objectClass=*)\" -w \"${__USER}\" "
 }
 
-nb-ad-ldap-whoami() {
+nb-enum-ldap-whoami() {
     __ask "Enter the address of the target DC, GC or LDAP server"
     nb-vars-set-rhost
     print -z "ldapwhoami -h ${__RHOST} -w \"non-existing-user\" "
 }
 
-nb-ad-ldap-hydra() {
+nb-enum-ldap-hydra() {
     __check-project
     nb-vars-set-rhost
     __check-user
