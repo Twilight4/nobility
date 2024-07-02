0d2d40dc7448a2ca0bc1a7f5476603d4da65b73c
Twilight4 <electrolight071@gmail.com>
2024-07-02 06:02:19 +0200

    update

--- modules/nb-install.zsh
+++ modules/nb-install.zsh
@@ -694,4 +694,5 @@ nb-install-windapsearch() {
 
 nb-install-arch-generic() {
     paru --needed -S nmap metasploit postgresql gobuster whatweb exploitdb masscan john bloodhound python-bloodhound python-neo4j sliver-bin hydra enum4linux smbmap crackmapexec
+    pip3 install impacket
 }
